57eac4d4947fa34494817aab3aaaa5fb986ef878
KMKoushik <koushikmohan1996@gmail.com>
2025-02-18 11:17:44 +1100

    add more git function

--- git_functions.sh
+++ git_functions.sh
@@ -3,11 +3,34 @@ push () {
 	git push origin "$branch"
 }
 
+p() {
+	push
+}
+
 fpush () {
 	push --force-with-lease
+}
+
+fp() {
+	fpush
 }
 
 commit() {
 	git add .
 	git commit -m "$1"
 }
+
+c() {
+	commit "$1"
+}
+
+rebase() {
+	git fetch
+	local branch=${1:-main}
+	git rebase origin/"$branch"
+}
+
+rb() {
+	rebase "$1"
+}
+
