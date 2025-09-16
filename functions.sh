f6494c210110558dd1ba59ceb224b9ed366e0c5c
KM Koushik <koushikmohan1996@gmail.com>
2025-09-16 16:27:16 +1000

    add codex function

--- functions.sh
+++ functions.sh
@@ -11,3 +11,8 @@ function o() {
 function csvcount() {
 	python -c "import csv, sys; csv.field_size_limit(sys.maxsize); print(sum(1 for row in csv.reader(sys.stdin)))" < "$1"
 }
+
+
+function cdx() {
+	codex -m gpt-5-codex
+}
